c1b85e43e9d2b578fe1c7076ae9b6f8882970768
Rene Moser <mail@renemoser.net>
2021-06-09 10:58:03 +0200

    cloudscale.ch: revamp

--- chaotic/cloud/cloudscale_ch.py
+++ chaotic/cloud/cloudscale_ch.py
@@ -19,19 +19,20 @@ class CloudscaleChChaotic(Chaotic):
         filter_tag = self.configs.get('filter_tag')
         log.info(f"Querying with filter_tag: {filter_tag}")
         servers = self.cloudscale.server.get_all(filter_tag=filter_tag)
+        if servers:
+            server = random.choice(servers)
+            log.info(f"Choose server {server['name']}")
+            if not self.dry_run:
+                log.info(f"Stopping server {server['name']}")
+                self.cloudscale.server.stop(uuid=server['uuid'])
 
-        server = random.choice(servers) if servers else None
-        if not server:
+                wait_before_restart = int(self.configs.get('wait_before_restart', 60))
+                log.info(f"Sleeping for server {wait_before_restart}")
+                time.sleep(wait_before_restart)
+
+                log.info(f"Starting server {wait_before_restart}")
+                self.cloudscale.server.start(uuid=server['uuid'])
+        else:
             log.info("No servers found")
-            return
-
-        log.info(f"Choose server {server['name']}")
-        if not self.dry_run:
-            log.info(f"Stopping server {server['name']}")
-            self.cloudscale.server.stop(uuid=server['uuid'])
-            wait_before_restart = int(self.configs.get('wait_before_restart', 60))
-            log.info(f"Sleeping for server {wait_before_restart}")
-            time.sleep(wait_before_restart)
-            self.cloudscale.server.start(uuid=server['uuid'])
 
         log.info(f"done")
