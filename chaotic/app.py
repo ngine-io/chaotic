276b62b52d6ee8fb5513a428c6ff79443d2e90b4
Rene Moser <mail@renemoser.net>
2021-06-09 10:57:39 +0200

    improve error handling

--- chaotic/app.py
+++ chaotic/app.py
@@ -1,20 +1,25 @@
 import os
 import yaml
+from chaotic.log import log
 
 from chaotic import ChaoticFactory
 
 def main() -> None:
-    config_file = os.getenv('CHAOTIC_CONFIG', 'config.yaml')
-    with open(config_file, "r") as infile:
-        config = yaml.load(infile, Loader=yaml.FullLoader)
 
-    chaos_factory = ChaoticFactory()
-    chaos = chaos_factory.get_instance(config['kind'])
-    chaos.configure(
-        configs=config.get('configs') or dict(),
-        dry_run=config.get('dry_run') or False
-    )
-    chaos.action()
+    try:
+        config_file = os.getenv('CHAOTIC_CONFIG', 'config.yaml')
+        with open(config_file, "r") as infile:
+            config = yaml.load(infile, Loader=yaml.FullLoader)
+
+        chaos_factory = ChaoticFactory()
+        chaos = chaos_factory.get_instance(config['kind'])
+        chaos.configure(
+            configs=config.get('configs') or dict(),
+            dry_run=config.get('dry_run') or False
+        )
+        chaos.action()
+    except Exception as ex:
+        log.error(ex)
 
 if __name__ == "__main__":
     main()
