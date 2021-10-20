784212d54f2411eadbe269d420c99483c449b5f8
Rene Moser <mail@renemoser.net>
2021-10-20 17:46:58 +0200

    improve json logging

--- chaotic/log.py
+++ chaotic/log.py
@@ -1,17 +1,23 @@
 import os
 import sys
 import logging
-
+from logging.config import fileConfig
 from dotenv import load_dotenv
 from pathlib import Path
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-logging.basicConfig(
-    stream=sys.stdout,
-    level=os.environ.get('CHAOTIC_LOG_LEVEL', 'INFO').upper(),
-    format='%(asctime)s - %(name)s:%(levelname)s:%(message)s')
+logging_config_file_path = os.environ.get('CHAOTIC_LOG_CONFIG', 'logging.ini')
 
-log = logging.getLogger('chatic')
+logging_config = Path(logging_config_file_path)
+if logging_config.is_file():
+    fileConfig(logging_config_file_path)
+else:
+    logging.basicConfig(
+        stream=sys.stdout,
+        level=os.environ.get('CHAOTIC_LOG_LEVEL', 'INFO').upper(),
+        format='%(asctime)s - %(name)s:%(levelname)s:%(message)s')
+
+log = logging.getLogger('chaotic')
 log.debug('Init')
