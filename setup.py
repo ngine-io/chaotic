1efca0c5752cf0bd92459b1a895d96dd20c04e75
René Moser <mail@renemoser.net>
2025-11-08 09:08:52 +0100

    add truststore, depends on py3.10+ (#169)

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     ],
     install_requires=install_requires,
     tests_require=tests_require,
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     entry_points={
         'console_scripts': [
             'chaotic-ngine = chaotic.app:main',
